ed37146e75d4dbb41f8f9f964e90900d3fc6c84a
Divyanshu6797 <divyanshu6797@gmail.com>
2024-05-27 17:56:11 +0530

    everything done

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import {useState, useEffect} from 'react';
 import io from 'socket.io-client';
 import {nanoid} from 'nanoid';
 
-const socket = io.connect("https://simple-chat-app-backend.vercel.app/", {
-  withCredentials: true,
+const socket = io.connect("https://simple-chat-app-backend.onrender.com/", {
+  
   
 });
-const userName = nanoid(4);
+let userName = nanoid(4);
 
 
 
 function App() {
   const[message, setMessage] = useState("");
+  const[userNameNew, setUserNameNew] = useState("");
+  
   const[chat, setChat] = useState([]);
 
   const sendChat = (e) => {
     e.preventDefault();
+    if(userNameNew.length > 0) {
+      userName = userNameNew
+    }
     socket.emit("chat", {message, userName});
 
     setMessage("");
@@ -40,6 +45,7 @@ function App() {
           ))
         }
        <form onSubmit={sendChat}>
+       <input type="text" placeholder='choose a username' value={userNameNew} onChange={(e) => setUserNameNew(e.target.value)} /> <br></br>
         <input type="text" name = "chat" placeholder='send text' value={message} onChange={(e) => setMessage(e.target.value)} />
         
        </form>
